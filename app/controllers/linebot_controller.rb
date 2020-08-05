5c178ddd40f11bbcec238a063a2d0f18474c1bc7
水澤嘉宏 <yoshihiromizusawa@mizusawayoshihironoMacBook.local>
2020-08-05 16:17:46 +0900

    add to linebot_text

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,22 +1,30 @@
 class LinebotController < ApplicationController
-  require 'line/bot'
+  # アルゴリズム
+  # １．LINEからのデータを入出力できるように認証登録する
+  # ２．LINEから送られてきたデータを入力
+  # ３．そのまま出力する
+  #
+  require 'line/bot' # gem 'line-bot-api'
 
   # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def callback
+
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
+
     unless client.validate_signature(body, signature)
       error 400 do 'Bad Request' end
     end
+
     events = client.parse_events_from(body)
 
     events.each do |event|
       case event
       when Line::Bot::Event::Message
         case event.type
-        when Line::Bot::Event::MessageType::Text
+        when Line::Bot::Event::Message::Text
           message = {
             type: 'text',
             text: event.message['text']
@@ -28,13 +36,14 @@ class LinebotController < ApplicationController
     head :ok
   end
 
-private
 
-# LINE Developers登録完了後に作成される環境変数の認証
+  private
+  # LINE Developers登録完了後に作成される環境変数の認証
   def client
-    @client ||= Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    @client ||= Line::Bot::Client.new { |config| 
+      config.CHANNEL_SECRET = ENV["LINE_CHANNEL_SECRET"]
+      config.CHANNEL_TOKEN = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
+  
 end
